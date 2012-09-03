2cb547fa24dfc3ea859be3d03c18c4e1945c9301
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2012-09-03 20:51:00 +0800

    Refactor the RuntimeErrors argument system.
    Change argument types to use an X-Macro system, so that new argument types are
    automatically added to switches over the argument types.
    
    Move the deserialization of arguments into the class for each argument type,
    and out of TraceReader.

--- lib/RuntimeErrors/UnicodeFormatter.cpp
+++ lib/RuntimeErrors/UnicodeFormatter.cpp
@@ -4,6 +4,7 @@
 #include "seec/RuntimeErrors/UnicodeFormatter.hpp"
 #include "seec/Util/Printing.hpp"
 
+#include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include "unicode/fmtable.h"
@@ -16,15 +17,15 @@
 namespace seec {
 
 namespace runtime_errors {
-  
+
 Formattable formatArg(ArgAddress const &A) {
   // TODO: convert address to string
   std::string AddressString;
-  
+
   llvm::raw_string_ostream AddressStringStream(AddressString);
   seec::util::write_hex_padded(AddressStringStream, A.address());
   AddressStringStream.flush();
-  
+
   return Formattable(AddressString.c_str());
 }
 
@@ -40,36 +41,43 @@ Formattable formatArg(ArgSize const &A) {
   // TODO: convert size to string
   return Formattable(static_cast<int64_t>(A.size()));
 }
-  
+
+Formattable formatArg(ArgOperand const &A) {
+  return Formattable(static_cast<int64_t>(A.index()));
+}
+
+Formattable formatArg(ArgParameter const &A) {
+  return Formattable(static_cast<int64_t>(A.index()));
+}
+
 Formattable formatArg(Arg const &A) {
   switch (A.type()) {
-    case ArgType::Address:
-      return formatArg(static_cast<ArgAddress const &>(A));
-    case ArgType::Object:
-      return formatArg(static_cast<ArgObject const &>(A));
-    case ArgType::Select:
-      return formatArg(static_cast<ArgSelectBase const &>(A));
-    case ArgType::Size:
-      return formatArg(static_cast<ArgSize const &>(A));
+    case ArgType::None:
+      break;
+#define SEEC_RUNERR_ARG(TYPE)                              \
+    case ArgType::TYPE:                                    \
+      return formatArg(static_cast<Arg##TYPE const &>(A));
+#include "seec/RuntimeErrors/ArgumentTypes.def"
   }
-  
+
+  llvm_unreachable("Unknown type");
   return Formattable();
 }
 
 UnicodeString format(RunError const &RunErr) {
   UnicodeString Result;
-  
+
   UErrorCode Status = U_ZERO_ERROR;
-  
+
   // get the entire RuntimeErrors bundle
   auto Resources = getResourceBundle("RuntimeErrors", Locale::getDefault());
   if (!Resources)
     return Result;
-  
+
   // get descriptions
   auto Descriptions = Resources->get("descriptions", Status);
   assert(U_SUCCESS(Status) && "Couldn't get Descriptions.");
-  
+
   // get error description
   auto FormatString = Descriptions.getStringEx(describe(RunErr.type()), Status);
   if (!U_SUCCESS(Status)) {
@@ -77,29 +85,29 @@ UnicodeString format(RunError const &RunErr) {
                  << ", Status = " << u_errorName(Status) << "\n";
     exit(EXIT_FAILURE);
   }
-  
+
   // For every argument, create a Formattable object and a name string.
   std::vector<UnicodeString> ArgumentNames;
   std::vector<Formattable> Arguments;
-  
+
   for (auto &Arg: RunErr.args()) {
     // Get the name for this argument.
     auto Name = getArgumentName(RunErr.type(), ArgumentNames.size());
-    
+
     ArgumentNames.push_back(UnicodeString::fromUTF8(Name));
     Arguments.push_back(formatArg(*Arg));
   }
-  
+
   MessageFormat Formatter(FormatString, Status);
   assert(U_SUCCESS(Status) && "Couldn't create MessageFormat object.");
-  
+
   Formatter.format(ArgumentNames.data(),
                    Arguments.data(),
                    Arguments.size(),
                    Result,
                    Status);
   assert(U_SUCCESS(Status) && "MessageFormat::format failed.");
-  
+
   return Result;
 }
 
