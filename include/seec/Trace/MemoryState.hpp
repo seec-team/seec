1a4ce678aa6da405db47e69e569ffecb4a9a4a93
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2012-09-05 13:27:43 +0800

    Add doxygen documentation for much of MemoryState, and add the MemoryState::Region class, which represents an interval in a MemoryState. This class doesn't do much currently, but we will use it when accessing information about a region of memory (e.g. querying the contents of alloca'd memory).

--- include/seec/Trace/MemoryState.hpp
+++ include/seec/Trace/MemoryState.hpp
@@ -23,7 +23,8 @@ namespace seec {
 namespace trace {
 
 
-/// \brief 
+/// \brief A fragment of a reconstructed memory state.
+///
 class MemoryStateFragment {
   /// The block of memory for this fragment.
   MappedMemoryBlock Block;
@@ -32,46 +33,68 @@ class MemoryStateFragment {
   EventLocation StateRecord;
   
 public:
-  /// Constructor.
+  /// \name Constructors
+  /// @{
+  
+  /// \brief Constructor.
   MemoryStateFragment(MappedMemoryBlock TheBlock, EventLocation TheEvent)
   : Block(std::move(TheBlock)),
     StateRecord(std::move(TheEvent))
   {}
 
-  /// Copy constructor.
+  /// \brief Copy constructor.
   MemoryStateFragment(MemoryStateFragment const &Other) = default;
 
-  /// Move constructor.
+  /// \brief Move constructor.
   MemoryStateFragment(MemoryStateFragment &&Other)
   : Block(std::move(Other.Block)),
     StateRecord(std::move(Other.StateRecord))
   {}
   
-  /// Copy assignment.
+  /// @} (Constructors)
+  
+  
+  /// \name Assignment
+  /// @{
+  
+  /// \brief Copy assignment.
   MemoryStateFragment &operator=(MemoryStateFragment const &RHS) = default;
   
-  /// Move assignment.
+  /// \brief Move assignment.
   MemoryStateFragment &operator=(MemoryStateFragment &&RHS) {
     Block = std::move(RHS.Block);
     StateRecord = std::move(RHS.StateRecord);
     return *this;
   }
   
+  /// @} (Assignment)
+  
+  
   /// \name Accessors
   /// @{
   
+  /// \brief Get a reference to the block of memory for this fragment.
   MappedMemoryBlock &getBlock() { return Block; }
   
+  /// \brief Get a const reference to the block of memory for this fragment.
   MappedMemoryBlock const &getBlock() const { return Block; }
   
+  /// \brief Get the EventLocation for the event that caused this state.
   EventLocation const &getStateRecordLocation() const { return StateRecord; }
   
-  /// @}
+  /// @} (Accessors)
 };
 
 
-/// \brief Represents the complete state of memory.
+/// \brief The complete reconstructed state of memory.
+/// 
+/// The memory state is stored as a collection of fragments. Each fragment
+/// represents the state caused by a single event, such as a store or memcpy().
+/// The fragments are kept in an ordered map, that maps the start address of
+/// the fragment to a MemoryStateFragment object.
+///
 class MemoryState {
+  /// Map fragments' start addresses to the fragments themselves.
   std::map<uint64_t, MemoryStateFragment> FragmentMap;
   
   // Don't allow copying
@@ -79,7 +102,7 @@ class MemoryState {
   MemoryState &operator=(MemoryState const &) = delete;
   
 public:
-  /// Constructor.
+  /// \brief Construct an empty MemoryState.
   MemoryState()
   : FragmentMap()
   {}
@@ -88,8 +111,12 @@ public:
   /// \name Accessors
   /// @{
   
+  /// \brief Get the map from start addresses to fragments.
+  ///
   decltype(FragmentMap) const &getFragmentMap() const { return FragmentMap; }
   
+  /// \brief Get the total number of fragments.
+  ///
   std::size_t getNumberOfFragments() const { return FragmentMap.size(); }
   
   /// @} (Accessors)
@@ -98,11 +125,65 @@ public:
   /// \name Mutators
   /// @{
   
+  /// \brief Add a memory block caused by an event.
+  ///
   void add(MappedMemoryBlock Block, EventLocation Event);
   
+  /// \brief Clear a region of memory.
+  ///
   void clear(MemoryArea Area);
   
   /// @} (Mutators)
+  
+  
+  /// \name Regions
+  /// @{
+  
+  /// \brief Represents a single region of MemoryState.
+  ///
+  class Region {
+    MemoryState const &State; ///< The state that this region belongs to.
+    
+    MemoryArea const Area; ///< The area that this region covers.
+  
+  public:
+    /// \name Constructors
+    /// @{
+    
+    /// \brief Construct a new Region covering an area in the given state.
+    ///
+    Region(MemoryState const &InState, MemoryArea RegionArea)
+    : State(InState),
+      Area(RegionArea)
+    {}
+    
+    /// \brief Copy constructor.
+    Region(Region const &) = default;
+    
+    /// \brief Move constructor.
+    Region(Region &&) = default;
+    
+    /// @} (Constructors)
+    
+    /// \name Accessors
+    /// @{
+    
+    /// \brief Get the state that this region belongs to.
+    MemoryState const &getState() const { return State; }
+    
+    /// \brief Get the area that this region covers.
+    MemoryArea const &getArea() const { return Area; }
+    
+    /// @}
+  };
+  
+  /// \brief Get a Region covering the given area.
+  ///
+  Region getRegion(MemoryArea Area) const {
+    return Region(*this, Area);
+  }
+  
+  /// @} (Regions)
 };
 
 /// Print a textual description of a MemoryState.
