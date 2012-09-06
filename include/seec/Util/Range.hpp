d65c0a7d2f0103577e130f4fe7e844c067e92968
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2012-09-06 21:16:18 +0800

    Updated doxygen comments.

--- include/seec/Util/Range.hpp
+++ include/seec/Util/Range.hpp
@@ -13,26 +13,35 @@
 
 namespace seec {
 
-/// A pair of iterators that can be used in a ranged for loop.
-/// \tparam IterT 
+/// \brief A pair of iterators that can be used in a ranged for loop.
+/// \tparam IterT The type of iterator used in the range.
 template<typename IterT>
 class Range
 {
-  IterT Begin;
-  
-  IterT End;
-  
+  IterT Begin; ///< Iterator for the first element in the range.
+
+  IterT End; ///< Iterator for the first element after the range.
+
 public:
+  /// \brief Construct a new range.
+  /// \param Begin Iterator for the first element in the range.
+  /// \param End Iterator for the first element after the range.
   Range(IterT Begin, IterT End)
   : Begin(Begin),
     End(End)
   {}
-  
+
+  /// \brief Get an iterator for the first element in the range.
   IterT begin() const { return Begin; }
-  
+
+  /// \brief Get an iterator for the first element after the range.
   IterT end() const { return End; }
 };
 
+/// Get a new range.
+/// \tparam IterT The type of iterator used in the range.
+/// \param Begin Iterator to the first element in the range.
+/// \param End Iterator to the first element after the range.
 template<typename IterT>
 Range<IterT> range(IterT Begin, IterT End) {
   return Range<IterT>(Begin, End);
--- include/seec/Util/Reverse.hpp
+++ include/seec/Util/Reverse.hpp
@@ -8,48 +8,84 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef _SEEC_UTIL_REVERSE_HPP_
-#define _SEEC_UTIL_REVERSE_HPP_
+#ifndef SEEC_UTIL_REVERSE_HPP
+#define SEEC_UTIL_REVERSE_HPP
 
 namespace seec {
 
-/// Adapt container types to be reversed for use in range for loops.
+/// \brief Adapt container types to be reversed for begin() and end().
+///
+/// This template class can be used to create a "reversed" view of a container,
+/// for use in range for loops and other places that support only forward
+/// iteration using begin() and end().
+///
+/// \tparam The type of container.
+///
 template<typename T>
 class ReverseAdaptor {
 private:
-  T &Container;
+  T &Container; ///< The container being adapted.
 
 public:
+  /// \brief Construct a new ReverseAdaptor for the given container.
   ReverseAdaptor(T &Container)
   : Container(Container)
   {}
 
+  /// \brief Get the beginning reverse iterator for the container.
+  /// \return Container.rbegin()
   typename T::reverse_iterator begin() { return Container.rbegin(); }
 
+  /// \brief Get the ending reverse iterator for the container.
+  /// \return Container.rend()
   typename T::reverse_iterator end() { return Container.rend(); }
 };
 
-/// Adapt const container types to be reversed for use in range for loops.
+/// \brief Adapt const container types to be reversed for begin() and end().
+///
+/// This template class can be used to create a "reversed" view of a container,
+/// for use in range for loops and other places that support only forward
+/// iteration using begin() and end().
+///
+/// \tparam The type of container.
+///
 template<typename T>
 class ReverseAdaptorConst {
 private:
-  T const &Container;
+  T const &Container; ///< The container being adapted.
 
 public:
+  /// \brief Construct a new ReverseAdaptorConst for the given const container.
   ReverseAdaptorConst(T const &Container)
   : Container(Container)
   {}
 
-  typename T::const_reverse_iterator begin() { return Container.rbegin(); }
+  /// \brief Get the beginning reverse iterator for the container.
+  /// \return Container.rbegin()
+  typename T::const_reverse_iterator begin() const {
+    return Container.rbegin();
+  }
 
-  typename T::const_reverse_iterator end() { return Container.rend(); }
+  /// \brief Get the ending reverse iterator for the container.
+  /// \return Container.rend()
+  typename T::const_reverse_iterator end() const {
+    return Container.rend();
+  }
 };
 
+/// \brief Get a "reversed" view of the given container.
+/// \tparam The type of container.
+/// \param Container a reference to the container.
+/// \return A ReverseAdaptor<T> for Container.
 template<typename T>
 ReverseAdaptor<T> reverse(T &Container) {
   return ReverseAdaptor<T>(Container);
 }
 
+/// \brief Get a "reversed" view of the given container.
+/// \tparam The type of container.
+/// \param Container a const reference to the container.
+/// \return A ReverseAdaptorConst<T> for Container.
 template<typename T>
 ReverseAdaptorConst<T> reverse(T const &Container) {
   return ReverseAdaptorConst<T>(Container);
@@ -57,4 +93,4 @@ ReverseAdaptorConst<T> reverse(T const &Container) {
 
 } // namespace seec
 
-#endif // _SEEC_UTIL_REVERSE_HPP_
+#endif // SEEC_UTIL_REVERSE_HPP
