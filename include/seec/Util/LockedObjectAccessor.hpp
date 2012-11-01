6ca4bfc4622fcf731e2997dd9e298696ed67af92
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2012-11-01 21:13:18 +0800

    Add LockedObjectAccessor template class.
    This template implements a class that holds a reference to an object, and a lock on a mutex that guards access to the object.
    While an instance of the class exists, it is legal for the owner to access the object.

---
+++ include/seec/Util/LockedObjectAccessor.hpp
@@ -0,0 +1,51 @@
+//===- include/seec/Util/LockedObjectAccessor.hpp ------------------- C++ -===//
+//
+//
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements a convenience class template for coupling a reference
+// to an object with a std::unique_lock that holds ownership to access that
+// object.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SEEC_UTIL_LOCKEDOBJECTACCESSOR_HPP
+#define SEEC_UTIL_LOCKEDOBJECTACCESSOR_HPP
+
+#include <mutex>
+
+namespace seec {
+
+template<typename ObjectT, typename MutexT>
+class LockedObjectAccessor {
+  std::unique_lock<MutexT> Lock;
+  
+  ObjectT &Object;
+
+public:
+  LockedObjectAccessor(MutexT &Mutex, ObjectT &Object)
+  : Lock(Mutex),
+    Object(Object)
+  {}
+  
+  LockedObjectAccessor(LockedObjectAccessor &&) = default;
+  
+  std::unique_lock<MutexT> const &getLock() const { return Lock; }
+  
+  ObjectT &getObject() const { return Object; }
+  
+  ObjectT &operator*() const { return Object; }
+  
+  ObjectT *operator->() const { return &Object; }
+};
+
+template<typename MutexT, typename ObjectT>
+LockedObjectAccessor<ObjectT, MutexT>
+makeLockedObjectAccessor(MutexT &Mutex, ObjectT &Object) {
+  return LockedObjectAccessor<ObjectT, MutexT>(Mutex, Object);
+}
+
+} // namespace seec
+
+#endif // SEEC_UTIL_LOCKEDOBJECTACCESSOR
