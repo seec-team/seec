e9ac6d5dd6b159dad8cd23055a1708dc10026e7b
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2012-11-05 16:28:52 +0800

    Add Argument indexing for indexed Functions.

--- include/seec/Util/ModuleIndex.hpp
+++ include/seec/Util/ModuleIndex.hpp
@@ -20,21 +20,23 @@
 
 namespace seec {
 
+/// \brief Index for an llvm::Function.
 class FunctionIndex {
-  // The indexed Function.
-  // llvm::Function &Function;
-
-  /// Lookup Instructions by their index.
+  /// Lookup Instruction pointers by their index.
   std::vector<llvm::Instruction *> InstructionPtrByIdx;
   
-  /// Map Instructions to their indices.
+  /// Map Instruction pointers to their indices.
   llvm::DenseMap<llvm::Instruction const *, uint32_t> InstructionIdxByPtr;
+  
+  /// Lookup Argument pointers by their index.
+  std::vector<llvm::Argument *> ArgumentPtrByIdx;
 
 public:
+  /// \brief Constructor.
   FunctionIndex(llvm::Function &Function)
-  : // Function(Function),
-    InstructionPtrByIdx(),
-    InstructionIdxByPtr()
+  : InstructionPtrByIdx(),
+    InstructionIdxByPtr(),
+    ArgumentPtrByIdx()
   {
     for (auto &BasicBlock: Function) {
       for (auto &Instruction: BasicBlock) {
@@ -43,12 +45,20 @@ public:
         InstructionPtrByIdx.push_back(&Instruction);
       }
     }
+    
+    for (auto &Argument : Function.getArgumentList()) {
+      ArgumentPtrByIdx.push_back(&Argument);
+    }
   }
+  
+  
+  /// \name Instruction mapping.
+  /// @{
 
-  /// Get the number of Instructions in the indexed Function.
+  /// \brief Get the number of Instructions in the indexed Function.
   size_t getInstructionCount() const { return InstructionPtrByIdx.size(); }
 
-  /// Get the Instruction at the given Index in the indexed Function.
+  /// \brief Get the Instruction at the given Index in the indexed Function.
   llvm::Instruction *getInstruction(uint32_t Index) const {
     if (Index < InstructionPtrByIdx.size())
       return InstructionPtrByIdx[Index];
@@ -66,10 +76,26 @@ public:
       return util::Maybe<uint32_t>(It->second);
     return util::Maybe<uint32_t>();
   }
+  
+  /// @}
+  
+  
+  /// \name Argument mapping.
+  /// @{
+  
+  /// \brief Get the Argument at the given Index in the indexed Function.
+  llvm::Argument *getArgument(uint32_t Index) const {
+    if (Index < ArgumentPtrByIdx.size())
+      return ArgumentPtrByIdx[Index];
+    return nullptr;
+  }
+  
+  /// @}
 };
 
+/// \brief Index for an llvm::Module.
 class ModuleIndex {
-  // The indexed Module.
+  /// The indexed Module.
   llvm::Module const &Module;
 
   /// Lookup GlobalVariables by their index.
@@ -92,6 +118,7 @@ class ModuleIndex {
   ModuleIndex &operator=(ModuleIndex const &RHS) = delete;
 
 public:
+  /// \brief Constructor.
   ModuleIndex(llvm::Module &Module,
               bool const GenerateFunctionIndexForAll = false)
   : Module(Module),
@@ -120,10 +147,10 @@ public:
     }
   }
   
-  /// Get the Module.
+  /// \brief Get the Module.
   llvm::Module const &getModule() const { return Module; }
   
-  /// Get the number of global variables in the indexed Module.
+  /// \brief Get the number of global variables in the indexed Module.
   size_t getGlobalCount() const { return GlobalPtrByIdx.size(); }
 
   /// Get the llvm::GlobalVariable at the given Index, or nullptr, if the Index
@@ -134,7 +161,7 @@ public:
     return nullptr;
   }
 
-  /// Get the Index of the given llvm::GlobalVariable.
+  /// \brief Get the Index of the given llvm::GlobalVariable.
   util::Maybe<uint32_t>
   getIndexOfGlobal(llvm::GlobalVariable const *Global) const {
     auto It = GlobalIdxByPtr.find(Global);
@@ -143,7 +170,7 @@ public:
     return util::Maybe<uint32_t>();
   }
   
-  /// Get the number of functions in the indexed Module.
+  /// \brief Get the number of functions in the indexed Module.
   size_t getFunctionCount() const { return FunctionPtrByIdx.size(); }
 
   /// Get the llvm::Function at the given Index, or nullptr, if the Index is
@@ -154,7 +181,7 @@ public:
     return nullptr;
   }
 
-  /// Get the Index of the given llvm::Function.
+  /// \brief Get the Index of the given llvm::Function.
   util::Maybe<uint32_t> getIndexOfFunction(llvm::Function const *Function) const {
     auto It = FunctionIdxByPtr.find(Function);
     if (It != FunctionIdxByPtr.end())
@@ -162,6 +189,7 @@ public:
     return util::Maybe<uint32_t>();
   }
 
+  /// \brief Generate the FunctionIndex for all llvm::Functions.
   void generateFunctionIndexForAll() const {
     for (std::size_t i = 0; i < FunctionIndexByIdx.size(); ++i) {
       if (!FunctionIndexByIdx[i]) {
@@ -171,6 +199,7 @@ public:
     }
   }
 
+  /// \brief Get the FunctionIndex for the llvm::Function with the given Index.
   FunctionIndex *getFunctionIndex(uint32_t Index) const {
     if (Index >= FunctionIndexByIdx.size())
       return nullptr;
@@ -184,6 +213,7 @@ public:
     return FunctionIndexByIdx[Index].get();
   }
 
+  /// \brief Get the FunctionIndex for the given llvm::Function.
   FunctionIndex *getFunctionIndex(llvm::Function const *Function) const {
     auto Idx = getIndexOfFunction(Function);
     if (!Idx.assigned())
