f6598b8aa430a9075100e3db371150576a3bb14b
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2012-11-15 16:12:21 +0800

    Modify event printing to allow overriding event printing for specific event types, and member printing for specific members of specific event types. Use these modifications to make RuntimeError's ErrorType print as the describe()'d error type.

--- lib/Trace/TraceFormat.cpp
+++ lib/Trace/TraceFormat.cpp
@@ -1,5 +1,6 @@
 #include "seec/Preprocessor/MakeMemberFnChecker.hpp"
 #include "seec/RuntimeErrors/ArgumentTypes.hpp"
+#include "seec/RuntimeErrors/RuntimeErrors.hpp"
 #include "seec/Trace/TraceFormat.hpp"
 #include "seec/Util/ConstExprMath.hpp"
 
@@ -12,43 +13,120 @@ namespace seec {
 namespace trace {
 
 
-class EventPrinter {
+/// \brief Default printing for event objects and primitives.
+class EventPrinterBase {
+protected:
+  /// The output stream we print to.
   llvm::raw_ostream &Out;
   
-public:
-  EventPrinter(llvm::raw_ostream &OutStream)
+  /// \brief Construct an EventPrinterBase that prints to OutStream.
+  /// \param OutStream the stream to print output to.
+  EventPrinterBase(llvm::raw_ostream &OutStream)
   : Out(OutStream)
   {}
   
-  EventPrinter &operator<<(unsigned char Value) {
-    Out << static_cast<unsigned int>(Value);
-    return *this;
-  }
-  
-  EventPrinter &operator<<(signed char Value) {
-    Out << static_cast<int>(Value);
-    return *this;
-  }
-  
+public:
+  /// \brief Forward all objects with unhandled types to the output stream.
+  /// \tparam T the type of the object.
+  /// \param Value the object.
   template<typename T>
-  EventPrinter &operator<<(T &&Value) {
+  EventPrinterBase &operator<<(T &&Value) {
     Out << std::forward<T>(Value);
     return *this;
   }
   
-  EventPrinter &changeColor(llvm::raw_ostream::Colors Color,
-                            bool Bold = false,
-                            bool BG = false) {
+  /// \brief Print unsigned char values as unsigned integers.
+  EventPrinterBase &operator<<(unsigned char Value) {
+    Out << static_cast<unsigned int>(Value);
+    return *this;
+  }
+  
+  /// \brief Print signed char values as integers.
+  EventPrinterBase &operator<<(signed char Value) {
+    Out << static_cast<int>(Value);
+    return *this;
+  }
+  
+  /// \brief Wrap llvm::raw_ostream::changeColor().
+  EventPrinterBase &changeColor(llvm::raw_ostream::Colors Color,
+                                bool Bold = false,
+                                bool BG = false) {
     Out.changeColor(Color, Bold, BG);
     return *this;
   }
   
-  EventPrinter &resetColor() {
+  /// \brief Wrap llvm::raw_ostream::resetColor().
+  EventPrinterBase &resetColor() {
     Out.resetColor();
     return *this;
   }
   
+  /// \brief Wrap llvm::raw_ostream::is_displayed().
   bool is_displayed() const { return Out.is_displayed(); }
+  
+  /// \brief Default printing scheme for members.
+  template<typename T>
+  EventPrinterBase &printMember(llvm::StringRef Name, T &&Value) {
+    if (is_displayed()) {
+      changeColor(llvm::raw_ostream::CYAN);
+      *this << " " << Name;
+      changeColor(llvm::raw_ostream::BLUE);
+      *this << "=";
+      changeColor(llvm::raw_ostream::WHITE);
+      *this << Value;
+      resetColor();
+    }
+    else {
+      *this << " " << Name << "=" << Value;
+    }
+    
+    return *this;
+  }
+};
+
+
+/// \brief Used to print event objects.
+class EventPrinter : public EventPrinterBase {
+public:
+  EventPrinter(llvm::raw_ostream &OutStream)
+  : EventPrinterBase(OutStream)
+  {}
+};
+
+
+/// \brief Implements default printing for event members.
+template<EventType ET>
+class MemberFormatterBase; // Will be specialized by X-Macro.
+
+
+/// \brief Handle printing for event members.
+/// This template class simply inherits the default implementations from
+/// MemberFormatterBase. Specialize the template to implement custom member
+/// printing for an event type.
+template<EventType ET>
+class MemberFormatter : public MemberFormatterBase<ET> {
+public:
+  MemberFormatter(EventPrinter &Out)
+  : MemberFormatterBase<ET>(Out)
+  {}
+};
+
+
+/// \brief Implements default printing for events.
+template<EventType ET>
+class EventFormatterBase; // Will be specialized by X-Macro.
+
+
+/// \brief Handle printing for events.
+/// This template class simply inherits the default implementation from
+/// EventFormatterBase. Specialize the template to implement custom printing
+/// for an event type.
+template<EventType ET>
+class EventFormatter : public EventFormatterBase<ET> {
+public:
+  EventFormatter(EventPrinter &Out)
+  : EventFormatterBase<ET>(Out)
+  {}
 };
 
 
@@ -67,8 +145,8 @@ char const *describe(EventType Type) {
 // RuntimeValueRecord
 //------------------------------------------------------------------------------
 
-EventPrinter &operator<<(EventPrinter &Out,
-                         RuntimeValueRecord const &Record) {
+EventPrinterBase &operator<<(EventPrinterBase &Out,
+                             RuntimeValueRecord const &Record) {
   return Out << "<union>";
 }
 
@@ -202,45 +280,93 @@ seec::util::Maybe<uint32_t> EventRecordBase::getIndex() const {
 
 
 //------------------------------------------------------------------------------
-// Event records
+// Default printing for event members.
 //------------------------------------------------------------------------------
 
-// Define raw_ostream output for all event records.
-#define SEEC_PP_MEMBER(TYPE, NAME)                                             \
-  if (Out.is_displayed()) {                                                    \
-    Out.changeColor(llvm::raw_ostream::CYAN);                                  \
-    Out << " " #NAME;                                                          \
-    Out.changeColor(llvm::raw_ostream::BLUE);                                  \
-    Out << "=";                                                                \
-    Out.changeColor(llvm::raw_ostream::WHITE);                                 \
-    Out << Event.get##NAME();                                                  \
-    Out.resetColor();                                                          \
-  }                                                                            \
-  else {                                                                       \
-    Out << " " #NAME "=" << Event.get##NAME();                                 \
-  }                                                                            \
+// Specialize MemberFormatterBase for each event type.
+#define SEEC_PP_MEMBER_PRINTER(TYPE, NAME)                                     \
+  void print##NAME(EventRecordTy const &Event) {                               \
+    Out.printMember(#NAME, Event.get##NAME());                                 \
+  }
 
 #define SEEC_TRACE_EVENT(NAME, MEMBERS, TRAITS)                                \
-EventPrinter &operator<<(EventPrinter &Out,                                    \
-                         EventRecord<EventType::NAME> const &Event) {          \
-  Out << "[";                                                                  \
-  if (Out.is_displayed()) {                                                    \
-    Out.changeColor(llvm::raw_ostream::GREEN);                                 \
-    Out << #NAME;                                                              \
-    Out.resetColor();                                                          \
-  }                                                                            \
-  else {                                                                       \
-    Out << #NAME;                                                              \
-  }                                                                            \
-  SEEC_PP_APPLY(SEEC_PP_MEMBER, MEMBERS)                                       \
-  Out << "]";                                                                  \
-  return Out;                                                                  \
-}
+template<>                                                                     \
+class MemberFormatterBase<EventType::NAME> {                                   \
+protected:                                                                     \
+  typedef EventRecord<EventType::NAME> EventRecordTy;                          \
+  EventPrinter &Out;                                                           \
+public:                                                                        \
+  MemberFormatterBase(EventPrinter &Out) : Out(Out) {}                         \
+  SEEC_PP_APPLY(SEEC_PP_MEMBER_PRINTER, MEMBERS)                               \
+};
 
 #include "seec/Trace/Events.def"
 
-#undef SEEC_PP_MEMBER
+#undef SEEC_PP_MEMBER_PRINTER
 
+
+//------------------------------------------------------------------------------
+// Specialized printing for event members.
+//------------------------------------------------------------------------------
+
+template<>
+class MemberFormatter<EventType::RuntimeError>
+: public MemberFormatterBase<EventType::RuntimeError> {
+public:
+  MemberFormatter(EventPrinter &Out)
+  : MemberFormatterBase<EventType::RuntimeError>(Out)
+  {}
+  
+  void printErrorType(EventRecord<EventType::RuntimeError> const &Event) {
+    Out.printMember("ErrorType",
+                    describe(static_cast<seec::runtime_errors::RunErrorType>
+                                        (Event.getErrorType())));
+  }
+};
+
+
+//------------------------------------------------------------------------------
+// Default printing for events.
+//------------------------------------------------------------------------------
+
+// Specialize EventFormatterBase for all event types.
+#define SEEC_PP_CALL_MEMBER_PRINTER(TYPE, NAME)                                \
+  MemberPrinter.print##NAME(Event);
+
+#define SEEC_TRACE_EVENT(NAME, MEMBERS, TRAITS)                                \
+template<>                                                                     \
+class EventFormatterBase<EventType::NAME> {                                    \
+  EventPrinter &Out;                                                           \
+public:                                                                        \
+  EventFormatterBase(EventPrinter &Out) : Out(Out) {}                          \
+  void print(EventRecord<EventType::NAME> const &Event) {                      \
+    Out << "[";                                                                \
+    if (Out.is_displayed()) {                                                  \
+      Out.changeColor(llvm::raw_ostream::GREEN);                               \
+      Out << #NAME;                                                            \
+      Out.resetColor();                                                        \
+    }                                                                          \
+    else {                                                                     \
+      Out << #NAME;                                                            \
+    }                                                                          \
+    MemberFormatter<EventType::NAME> MemberPrinter(Out);                       \
+    SEEC_PP_APPLY(SEEC_PP_CALL_MEMBER_PRINTER, MEMBERS)                        \
+    Out << "]";                                                                \
+  }                                                                            \
+};
+
+#include "seec/Trace/Events.def"
+
+#undef SEEC_PP_CALL_MEMBER_PRINTER
+
+//------------------------------------------------------------------------------
+// Specialized printing for events.
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+// Check that all event records meet the requirements.
+//------------------------------------------------------------------------------
 
 #if 0
 static_assert(std::is_trivially_copyable<EventRecordBase>::value,
@@ -273,22 +399,33 @@ static_assert(                                                                 \
 
 
 //------------------------------------------------------------------------------
+// EventRecord output for llvm::raw_ostream
+//------------------------------------------------------------------------------
+
+#define SEEC_TRACE_EVENT(NAME, MEMBERS, TRAITS)                                \
+llvm::raw_ostream &operator<<(llvm::raw_ostream &Out,                          \
+                              EventRecord<EventType::NAME> const &Event) {     \
+  EventPrinter Printer(Out);                                                   \
+  EventFormatter<EventType::NAME> Formatter(Printer);                          \
+  Formatter.print(Event);                                                      \
+  return Out;                                                                  \
+}
+#include "seec/Trace/Events.def"
+
+//------------------------------------------------------------------------------
 // EventRecordBase output for llvm::raw_ostream
 //------------------------------------------------------------------------------
 
-llvm::raw_ostream & operator<<(llvm::raw_ostream &OutStream,
-                               EventRecordBase const &Event) {
-  EventPrinter Printer(OutStream);
-  
+llvm::raw_ostream &operator<<(llvm::raw_ostream &OutStream,
+                              EventRecordBase const &Event) {
   switch (Event.getType()) {
     case EventType::None:
       return OutStream << "<none>";
 
 #define SEEC_TRACE_EVENT(NAME, MEMBERS, TRAITS)                                \
     case EventType::NAME:                                                      \
-      Printer << static_cast<EventRecord<EventType::NAME> const &>(Event);     \
-      return OutStream;
-
+      return OutStream                                                         \
+             << static_cast<EventRecord<EventType::NAME> const &>(Event);
 #include "seec/Trace/Events.def"
 
     default:
