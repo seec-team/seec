6389ccf620884a392b71cfb85cb2a4d1e43a9cef
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2012-11-20 21:43:54 +0800

    Add utility header for dealing with compile-time sequences.

---
+++ include/seec/Util/TemplateSequence.hpp
@@ -0,0 +1,41 @@
+//===- Util/TemplateSequence.hpp ------------------------------------ C++ -===//
+//
+//
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SEEC_UTIL_TEMPLATESEQUENCE_HPP
+#define SEEC_UTIL_TEMPLATESEQUENCE_HPP
+
+namespace seec {
+
+namespace compile_time {
+
+/// \brief A compile-time sequence of ints.
+template<int ...>
+struct sequence_int {};
+
+/// \brief Build a compile-time sequence of ints to cover a range.
+/// Builds a sequence [Start, End) by recursively instantiating itself and
+/// accumulating in the Sequence parameter pack.
+template<int Start, int End, int ...Sequence>
+struct generate_sequence_int
+: public generate_sequence_int<Start, End - 1, End - 1, Sequence...> {};
+
+/// \brief Build a compile-time sequence of ints to cover a range.
+/// Base case of the recursion, create a sequence using all values that have
+/// accumulated in the Sequence parameter pack.
+template<int StartEnd, int ...Sequence>
+struct generate_sequence_int<StartEnd, StartEnd, Sequence...> {
+  typedef sequence_int<Sequence...> type;
+};
+
+} // namespace compile_time
+
+} // namespace seec
+
+#endif // SEEC_UTIL_TEMPLATESEQUENCE_HPP
