7dd0fdced6e0473b14303b8213167cb7fb05a0d7
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2012-12-03 09:56:06 +0800

    Add operator== for comparing two clang::const_strings::StringRef objects.

--- include/seec/Util/ConstExprCString.hpp
+++ include/seec/Util/ConstExprCString.hpp
@@ -17,12 +17,23 @@ namespace seec {
 /// Functions for performing constexpr c-string operations.
 namespace const_strings {
 
+
 class StringRef {
-  char const * Begin;
+  char const * const Begin;
   
-  unsigned Size;
+  unsigned const Size;
   
 public:
+  constexpr StringRef()
+  : Begin(nullptr),
+    Size(0)
+  {}
+  
+  constexpr StringRef(char const * const StringBegin, unsigned const StringSize)
+  : Begin(StringBegin),
+    Size(StringSize)
+  {}
+  
   template<unsigned N>
   constexpr StringRef(char const(&Str)[N])
   : Begin(Str),
@@ -30,6 +41,10 @@ public:
   {
     static_assert(N >= 1, "not a string literal");
   }
+  
+  
+  /// \name Accessors.
+  /// @{
   
   constexpr char operator[](unsigned i) {
     return Begin[i];
@@ -42,17 +57,47 @@ public:
   constexpr unsigned size() {
     return Size;
   }
+  
+  /// @}
+  
+  
+  /// \name Create mutated copies.
+  /// @{
+  
+  constexpr StringRef takeAllFromIndex(unsigned const i) {
+    return i >= size() ? StringRef()
+                       : StringRef(Begin + i, size() - i);
+  }
+  
+  /// @}
 };
 
-constexpr bool contains(StringRef Str, char c, unsigned i) {
+
+/// \brief Returns true iff the char c is contained in Str at or after index i.
+///
+constexpr bool contains(StringRef const Str, char const c, unsigned const i) {
   return i >= Str.size() ? false
                          : Str[i] == c ? true
                                        : contains(Str, c, i + 1);
 }
 
-constexpr bool contains(StringRef Str, char c) {
+
+/// \brief Returns true iff the char c is contained in Str.
+///
+constexpr bool contains(StringRef const Str, char const c) {
   return contains(Str, c, 0);
 }
+
+
+/// \brief Returns true iff LHS and RHS have the same contents.
+///
+constexpr bool operator==(StringRef const LHS, StringRef const RHS) {
+  return LHS.size() == 0 ? RHS.size() == 0
+                         : LHS[0] != RHS[0] ? false
+                                            : LHS.takeAllFromIndex(1)
+                                              == RHS.takeAllFromIndex(1);
+}
+
 
 } // namespace const_strings (in seec)
 
