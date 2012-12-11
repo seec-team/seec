8223dd2a7e0460112dde12c585f52d4af0de71d1
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2012-12-11 14:07:52 +0800

    Unify checking and recording of scanf and fscanf into CheckStreamScan().

--- lib/Runtimes/Tracer/WrapCstdio_h.cpp
+++ lib/Runtimes/Tracer/WrapCstdio_h.cpp
@@ -13,8 +13,12 @@
 
 #include "Tracer.hpp"
 
+#include "seec/RuntimeErrors/FormatSelects.hpp"
 #include "seec/Runtimes/MangleFunction.h"
+#include "seec/Trace/DetectCalls.hpp"
 #include "seec/Trace/TraceThreadListener.hpp"
+#include "seec/Trace/TraceThreadMemCheck.hpp"
+#include "seec/Util/ScopeExit.hpp"
 
 #include "llvm/Support/CallSite.h"
 
@@ -23,51 +27,82 @@
 
 extern "C" {
 
+//===----------------------------------------------------------------------===//
+// scanf, fscanf, sscanf
+//===----------------------------------------------------------------------===//
+
+static int
+CheckStreamScan(seec::runtime_errors::format_selects::CStdFunction FSFunction,
+                unsigned VarArgsStartIndex,
+                FILE *Stream,
+                char const *Format)
+{
+  auto &ThreadEnv = seec::trace::getThreadEnvironment();
+  auto &Listener = ThreadEnv.getThreadListener();
+  auto Instruction = ThreadEnv.getInstruction();
+  auto InstructionIndex = ThreadEnv.getInstructionIndex();
+  auto Call = llvm::CallSite(Instruction);
+  assert(Call && "expected call or invoke instruction.");
+    
+  // Interace with the thread listener's notification system.
+  Listener.enterNotification();
+  auto DoExit = seec::scopeExit([&](){ Listener.exitPostNotification(); });
+  
+  // Use a VarArgList to access our arguments.
+  seec::trace::detect_calls::VarArgList<seec::trace::TraceThreadListener>
+    VarArgs{Listener, Call, VarArgsStartIndex};
+
+  // Lock IO streams and global memory.
+  Listener.acquireGlobalMemoryWriteLock();
+  auto StreamsAccessor = Listener.getProcessListener().getStreamsAccessor();
+  
+  // Use a CIOChecker to help check memory.
+  seec::trace::CIOChecker Checker{Listener,
+                                  InstructionIndex,
+                                  FSFunction,
+                                  StreamsAccessor.getObject()};
+  
+  // Check that the stream is valid.
+  if (Stream == stdin || Stream == stdout || Stream == stderr) {
+    Checker.checkStandardStreamIsValid(Stream);
+  }
+  else {
+    Checker.checkStreamIsValid(0, Stream);
+  }
+  
+  // TODO: Check and do the scanf.
+  auto FormatSize = Checker.checkCStringRead(VarArgsStartIndex - 1, Format);
+  if (!FormatSize)
+    return 0;
+  
+  int Result = 0;
+  
+  // Record the produced value.
+  Listener.notifyValue(InstructionIndex, Instruction, unsigned(Result));
+  
+  return Result;
+}
+
 int
 SEEC_MANGLE_FUNCTION(scanf)
 (char const *Format, ...)
 {
-  auto &ThreadEnv = seec::trace::getThreadEnvironment();
-  auto &Listener = ThreadEnv.getThreadListener();
-  auto Call = llvm::CallSite(ThreadEnv.getInstruction());
-  assert(Call && "expected call or invoke instruction.");
-  
-  Listener.enterNotification();
-  Listener.acquireGlobalMemoryWriteLock();
-  Listener.acquireStreamsLock();
-  
-  // Check and do the scanf.
-  int Result = 0;
-  
-  // Release memory and streams.
-  Listener.exitPostNotification();
-  
-  // Record the produced value.
-  Listener.notifyValue(ThreadEnv.getInstructionIndex(),
-                       ThreadEnv.getInstruction(),
-                       unsigned(Result));
-  
-  return Result;
+  return CheckStreamScan
+            (seec::runtime_errors::format_selects::CStdFunction::scanf,
+             1,
+             stdin,
+             Format);
 }
 
 int
 SEEC_MANGLE_FUNCTION(fscanf)
 (FILE *Stream, char const *Format, ...)
 {
-  auto &ThreadEnv = seec::trace::getThreadEnvironment();
-  auto &Listener = ThreadEnv.getThreadListener();
-  auto Call = llvm::CallSite(ThreadEnv.getInstruction());
-  assert(Call && "expected call or invoke instruction.");
-    
-  // TODO: Check and do.
-  
-  // Do.
-  llvm_unreachable("fscanf: not implemented.");
-  
-  // Record.
-  
-  // Return.
-  return 0;
+  return CheckStreamScan
+            (seec::runtime_errors::format_selects::CStdFunction::fscanf,
+             2,
+             Stream,
+             Format);
 }
 
 int
