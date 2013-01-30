bc9a0f40c3acfc363426c9bb62f3425c686ceb4c
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2013-01-30 15:37:21 +0800

    Add seec::seec_clang::search(), which takes a clang::ASTUnit, a filename, a line, and a column, and attempts to find the "most specific" AST node at the given position.

---
+++ include/seec/Clang/Search.hpp
@@ -0,0 +1,81 @@
+//===- include/seec/Clang/Search.hpp --------------------------------------===//
+//
+//                                    SeeC
+//
+// This file is distributed under The MIT License (MIT). See LICENSE.TXT for
+// details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SEEC_CLANG_SEARCH_HPP
+#define SEEC_CLANG_SEARCH_HPP
+
+
+#include "seec/Util/Error.hpp"
+#include "seec/Util/Maybe.hpp"
+
+#include "llvm/ADT/StringRef.h"
+
+
+namespace clang {
+  class ASTUnit;
+  class Decl;
+  class Stmt;
+}
+
+
+namespace seec {
+
+namespace seec_clang {
+
+
+class SearchResult {
+public:
+  enum class EFoundKind {
+    None,
+    Decl,
+    Stmt
+  };
+  
+private:
+  ::clang::Decl *FoundDecl;
+  
+  ::clang::Stmt *FoundStmt;
+  
+  EFoundKind FoundLast;
+  
+public:
+  SearchResult(::clang::Decl *Decl,
+               ::clang::Stmt *Stmt,
+               EFoundKind Last)
+  : FoundDecl(Decl),
+    FoundStmt(Stmt),
+    FoundLast(Last)
+  {}
+  
+  /// \name Accessors.
+  /// @{
+  
+  ::clang::Decl *getFoundDecl() const { return FoundDecl; }
+  
+  ::clang::Stmt *getFoundStmt() const { return FoundStmt; }
+  
+  EFoundKind getFoundLast() const { return FoundLast; }
+  
+  /// @}
+};
+
+
+seec::util::Maybe<SearchResult, seec::Error>
+search(::clang::ASTUnit &AST,
+       llvm::StringRef Filename,
+       unsigned Line,
+       unsigned Column);
+
+
+} // namespace seec_clang (in seec)
+
+} // namespace seec
+
+
+#endif // SEEC_CLANG_SEARCH_HPP
---
+++ lib/Clang/Search.cpp
@@ -0,0 +1,154 @@
+//===- lib/Clang/Search.cpp -----------------------------------------------===//
+//
+//                                    SeeC
+//
+// This file is distributed under The MIT License (MIT). See LICENSE.TXT for
+// details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+///
+//===----------------------------------------------------------------------===//
+
+#include "seec/Clang/Search.hpp"
+
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Frontend/ASTUnit.h"
+
+
+namespace seec {
+
+namespace seec_clang {
+
+
+class SearchingASTVisitor
+: public clang::RecursiveASTVisitor<SearchingASTVisitor>
+{
+  clang::SourceManager &SMgr;
+  
+  clang::SourceLocation SLoc;
+  
+  clang::Decl *FoundDecl;
+  
+  clang::Stmt *FoundStmt;
+  
+  SearchResult::EFoundKind FoundLast;
+  
+public:
+  SearchingASTVisitor(clang::SourceManager &SrcMgr,
+                      clang::SourceLocation SearchLocation)
+  : SMgr(SrcMgr),
+    SLoc(SearchLocation),
+    FoundDecl(nullptr),
+    FoundStmt(nullptr),
+    FoundLast(SearchResult::EFoundKind::None)
+  {}
+  
+  bool VisitStmt(clang::Stmt *S) {
+    auto Range = S->getSourceRange();
+    if (Range.isInvalid())
+      return true;
+    
+    if (SMgr.isBeforeInTranslationUnit(SLoc, Range.getBegin())) {
+      return false;
+    }
+    
+    if (SMgr.isBeforeInTranslationUnit(Range.getEnd(), SLoc)) {
+      return true;
+    }
+    
+    FoundStmt = S;
+    FoundLast = SearchResult::EFoundKind::Stmt;
+    return true;
+  }
+  
+  bool VisitDecl(clang::Decl *D) {
+    auto Range = D->getSourceRange();
+    if (Range.isInvalid())
+      return true;
+    
+    if (SMgr.isBeforeInTranslationUnit(SLoc, Range.getBegin())) {
+      return false;
+    }
+    
+    if (SMgr.isBeforeInTranslationUnit(Range.getEnd(), SLoc)) {
+      return true;
+    }
+    
+    FoundDecl = D;
+    FoundLast = SearchResult::EFoundKind::Decl;
+    return true;
+  }
+  
+  clang::Decl *getFoundDecl() const { return FoundDecl; }
+  
+  clang::Stmt *getFoundStmt() const { return FoundStmt; }
+  
+  SearchResult::EFoundKind getFoundLast() const { return FoundLast; }
+};
+
+
+seec::util::Maybe<SearchResult, seec::Error>
+search(::clang::ASTUnit &AST,
+       llvm::StringRef Filename,
+       unsigned Line,
+       unsigned Column)
+{
+  auto &FileMgr = AST.getFileManager();
+    
+  auto const File = FileMgr.getFile(Filename, false);
+  if (!File)
+    return seec::Error(LazyMessageByRef::create("SeeCClang",
+                                                {"errors",
+                                                 "FileManagerGetFileFail"}));
+  
+  auto &SourceMgr = AST.getSourceManager();
+  auto const FileID = SourceMgr.translateFile(File);
+  auto const SLoc = SourceMgr.translateLineCol(FileID, Line, Column);
+  
+  if (SLoc.isInvalid())
+    return seec::Error(LazyMessageByRef::create("SeeCClang",
+                                                {"errors",
+                                                 "SourceLocationInvalid"}));
+  
+  llvm::SmallVector<::clang::Decl *, 8> FoundDecls;
+  AST.findFileRegionDecls(FileID,
+                          SourceMgr.getFileOffset(SLoc),
+                          0,
+                          FoundDecls);
+  
+  if (FoundDecls.empty())
+    return SearchResult(nullptr, nullptr, SearchResult::EFoundKind::None);
+  
+  for (auto &Decl : FoundDecls) {
+    if (Decl->getSourceRange().isInvalid())
+      continue;
+    
+    // isInLexicalContext?
+    
+    if (clang::TagDecl *TD = llvm::dyn_cast<clang::TagDecl>(Decl))
+      if (!TD->isFreeStanding())
+        continue;
+    
+    // If Decl is before SLoc, continue.
+    // If Decl is after SLoc, break.
+    
+    SearchingASTVisitor Visitor (SourceMgr, SLoc);
+    Visitor.TraverseDecl(Decl);
+    
+    if (Visitor.getFoundLast() != SearchResult::EFoundKind::None)
+      return SearchResult(Visitor.getFoundDecl(),
+                          Visitor.getFoundStmt(),
+                          Visitor.getFoundLast());
+                        
+  }
+  
+  return SearchResult(nullptr, nullptr, SearchResult::EFoundKind::None);
+}
+
+
+} // namespace seec_clang (in seec)
+
+} // namespace seec
