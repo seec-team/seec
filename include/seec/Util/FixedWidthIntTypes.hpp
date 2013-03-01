68cb245f3ce667870f812b0f69c88a7cdd1af637
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2013-03-01 16:33:42 +0800

    Add a convenience template, GetFixedWidthInt<typename T>, which evaluates to a struct with a typedef named "type", which has a fixed-width integer type that is exactly the size and signedness of T.

--- include/seec/Util/FixedWidthIntTypes.hpp
+++ include/seec/Util/FixedWidthIntTypes.hpp
@@ -32,6 +32,7 @@ template<> struct GetInt<16> { typedef int16_t type; };
 template<> struct GetInt<32> { typedef int32_t type; };
 template<> struct GetInt<64> { typedef int64_t type; };
 
+
 //===----------------------------------------------------------------------===//
 // GetUInt<N>
 //===----------------------------------------------------------------------===//
@@ -42,6 +43,27 @@ template<> struct GetUInt<8>  { typedef uint8_t  type; };
 template<> struct GetUInt<16> { typedef uint16_t type; };
 template<> struct GetUInt<32> { typedef uint32_t type; };
 template<> struct GetUInt<64> { typedef uint64_t type; };
+
+
+//===----------------------------------------------------------------------===//
+// GetFixedWidthInt<T>
+//===----------------------------------------------------------------------===//
+
+template<typename T, typename Enable = void> struct GetFixedWidthInt;
+
+template<typename T>
+struct
+GetFixedWidthInt<T, typename std::enable_if<std::is_signed<T>::value>::type>
+{
+  typedef typename GetInt<sizeof(T) * CHAR_BIT>::type type;
+};
+
+template<typename T>
+struct
+GetFixedWidthInt<T, typename std::enable_if<std::is_unsigned<T>::value>::type>
+{
+  typedef typename GetUInt<sizeof(T) * CHAR_BIT>::type type;
+};
 
 
 } // namespace seec
