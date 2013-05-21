22a748a85f9223f041cbc9fd48d8da89eaf9ced3
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2013-05-21 12:55:42 +0800

    Make constructors and assignment operators for Interval and MemoryArea noexcept.
    Also generally update some of the comments to suit the current style.

--- include/seec/DSA/Interval.hpp
+++ include/seec/DSA/Interval.hpp
@@ -22,6 +22,7 @@ namespace seec {
 
 /// \brief Defines an interval of the form [ValueT, ValueT).
 /// \tparam ValueT the type of value used for the boundaries of the interval.
+///
 template<typename ValueT>
 class Interval {
   /// The first value that is a part of the interval.
@@ -32,6 +33,7 @@ class Interval {
 
   /// Constructor.
   Interval(ValueT StartValue, ValueT EndValue)
+  noexcept
   : Start(StartValue),
     End(EndValue)
   {}
@@ -40,17 +42,24 @@ public:
   /// \name Constructors.
   /// @{
 
-  /// Copy constructor.
-  Interval(Interval const &RHS) = default;
+  /// \brief Copy constructor.
+  ///
+  Interval(Interval const &RHS) noexcept = default;
 
-  /// Construct a new interval by specifying the start and end.
-  static Interval withStartEnd(ValueT StartValue, ValueT EndValue) {
+  /// \brief Construct a new interval by specifying the start and end.
+  ///
+  static Interval withStartEnd(ValueT StartValue, ValueT EndValue)
+  noexcept
+  {
     assert(EndValue >= StartValue);
     return Interval(StartValue, EndValue);
   }
 
-  /// Construct a new interval by specifying the start and length.
-  static Interval withStartLength(ValueT StartValue, ValueT LengthValue) {
+  /// \brief Construct a new interval by specifying the start and length.
+  ///
+  static Interval withStartLength(ValueT StartValue, ValueT LengthValue)
+  noexcept
+  {
     assert(LengthValue >= 0);
     return Interval(StartValue, StartValue + LengthValue);
   }
@@ -61,7 +70,8 @@ public:
   /// \name Assignment.
   /// @{
 
-  /// Copy assignment.
+  /// \brief Copy assignment.
+  ///
   Interval &operator=(Interval const &RHS) = default;
 
   /// @} (Assignment)
@@ -71,9 +81,11 @@ public:
   /// @{
 
   /// \brief Get the first value in the interval.
+  ///
   ValueT const &start() const { return Start; }
 
   /// \brief Get the first value not in the interval.
+  ///
   ValueT const &end() const { return End; }
 
   /// @} (Accessors)
@@ -83,18 +95,21 @@ public:
   /// @{
 
   /// \brief Set a new Start value for the interval.
+  ///
   void setStart(ValueT Value) {
     assert(Value <= End);
     Start = Value;
   }
 
   /// \brief Set a new End value for the interval.
+  ///
   void setEnd(ValueT Value) {
     assert(Value >= Start);
     End = Value;
   }
 
   /// \brief Set new Start and End values for the interval.
+  ///
   void setStartEnd(ValueT StartValue, ValueT EndValue) {
     assert(EndValue >= StartValue);
     Start = StartValue;
@@ -102,12 +117,14 @@ public:
   }
 
   /// \brief Get a copy of this Interval with start equal to Value.
+  ///
   Interval withStart(ValueT Value) const {
     assert(Value <= End);
     return withStartEnd(Value, End);
   }
 
   /// \brief Get a copy of this Interval with end equal to Value.
+  ///
   Interval withEnd(ValueT Value) const {
     assert(Value >= Start);
     return withStartEnd(Start, Value);
@@ -120,9 +137,11 @@ public:
   /// @{
 
   /// \brief Get the length of the interval.
+  ///
   ValueT length() const { return End - Start; }
 
   /// \brief Get the last value in the interval (for integral ValueT only).
+  ///
   typename std::enable_if<std::is_integral<ValueT>::value, ValueT>::type
   last() const {
     if (Start != End)
@@ -131,20 +150,25 @@ public:
   }
 
   /// \brief Check if a value is contained in this interval.
+  ///
   bool contains(ValueT Value) const {
     return Start <= Value && Value < End;
   }
 
   /// \brief Check if another interval is completely contained in this interval.
+  ///
   bool contains(Interval const &Other) const {
     return Start <= Other.Start && End >= Other.End;
   }
 
   /// \brief Check if another interval intersects this interval.
+  ///
   bool intersects(Interval const &Other) const {
     return Other.End > Start && Other.Start < End;
   }
 
+  /// \brief Get the intersection of this interval with another interval.
+  ///
   Interval<ValueT> intersection(Interval const &Other) const {
     // If there is no intersection, return an empty Interval.
     if (!intersects(Other))
@@ -156,10 +180,14 @@ public:
     return Interval<ValueT>::withStartEnd(IntersectStart, IntersectEnd);
   }
   
+  /// \brief Check if this interval is equal to another interval.
+  ///
   bool operator==(Interval const &RHS) const {
     return Start == RHS.Start && End == RHS.End;
   }
   
+  /// \brief Check if this interval is not equal to another interval
+  ///
   bool operator!=(Interval const &RHS) const {
     return Start != RHS.Start || End != RHS.End;
   }
