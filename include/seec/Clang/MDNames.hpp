7cc7a215c61fe8550c01214a527f939c54d0c0a6
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2013-06-10 15:23:15 +0800

    Add serialization for SeeC-Clang's parameter-decl mapping information.

--- include/seec/Clang/MDNames.hpp
+++ include/seec/Clang/MDNames.hpp
@@ -18,7 +18,8 @@ char const * const MDDeclIdxStr = "seec.clang.decl.idx";
 char const * const MDGlobalDeclPtrsStr = "clang.global.decl.ptrs";
 char const * const MDGlobalDeclIdxsStr = "seec.clang.global.decl.idxs";
 
-char const * const MDGlobalValueMapStr = "seec.clang.map.idxs";
+char const * const MDGlobalValueMapStr = "seec.clang.map.stmt.idxs";
+char const * const MDGlobalParamMapStr = "seec.clang.map.param.idxs";
 
 char const * const MDCompileInfo = "seec.clang.compile.info";
 
