441b4ff5d1e2b2d3ae26c3db8130f73bbc585b30
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2013-06-20 16:22:43 +0800

    Modify forward movement of SeeC-Clang mapped thread states to attempt to move in logical steps, rather than exposing the underlying LLVM IR state changes to the user.

--- lib/Clang/MappedStateMovement.cpp
+++ lib/Clang/MappedStateMovement.cpp
@@ -12,9 +12,11 @@
 //===----------------------------------------------------------------------===//
 
 #include "seec/Clang/MappedProcessState.hpp"
+#include "seec/Clang/MappedProcessTrace.hpp"
 #include "seec/Clang/MappedStateMovement.hpp"
 #include "seec/Clang/MappedThreadState.hpp"
 #include "seec/Trace/StateMovement.hpp"
+#include "seec/Trace/ThreadState.hpp"
 
 
 namespace seec {
@@ -25,10 +27,85 @@ namespace cm {
 //===----------------------------------------------------------------------===//
 // Thread movement.
 
+void moveToEndOfStmt(seec::trace::ThreadState &Unmapped,
+                     seec::seec_clang::MappedModule const &Mapping,
+                     llvm::Instruction const &Current)
+{
+  using namespace seec::trace;
+  
+  while (auto const Next = getNextInstructionInActiveFunction(Unmapped)) {
+    if (!Mapping.areMappedToSameStmt(Current, *Next))
+      return;
+    if (!seec::trace::moveForward(Unmapped))
+      return;
+  }
+}
+
 bool moveForward(ThreadState &Thread) {
-  auto const Success = seec::trace::moveForward(Thread.getUnmappedState());
+  // Move forward until we reach the next "logical" state.
+  //
+  // - If we enter a new Function, we have moved to a new "logical state", but
+  //   we should not stop until the prelude is complete. We will treat the last
+  //   unmapped Instruction as the end of the prelude.
+  //
+  // - If the active Stmt changes then we have moved to a new "logical state",
+  //   but we should move to the last Instruction that is mapped to this new
+  //   Stmt, so that a block belonging to a Stmt appears to execute in one step.
+  //
+  // - If we exit a Function then this is a new "logical" state.
+  
+  auto &Unmapped = Thread.getUnmappedState();
+  auto const &Trace = Thread.getParent().getProcessTrace();
+  auto const &MappedModule = Trace.getMapping();
+  
+  auto const InitialStackSize = Unmapped.getCallStack().size();
+  auto const InitialActiveFn = Unmapped.getActiveFunction();
+  
+  bool Moved = false;
+  
+  while (seec::trace::moveForward(Unmapped)) {
+    Moved = true;
+    
+    auto const ActiveFn = Unmapped.getActiveFunction();
+    
+    if (ActiveFn == InitialActiveFn) {
+      auto const ActiveInst = ActiveFn->getActiveInstruction();
+      if (!ActiveInst || !MappedModule.isMappedToStmt(*ActiveInst))
+        continue;
+      
+      // We're now on a new mapped Stmt. Move forward as far as possible while
+      // maintaining this as the active Stmt, and then we're done.
+      moveToEndOfStmt(Unmapped, MappedModule, *ActiveInst);
+    }
+    else {
+      auto const StackSize = Unmapped.getCallStack().size();
+      
+      // Function has completed, move to the end of the parent function's
+      // active Stmt.
+      if (StackSize < InitialStackSize) {
+        if (ActiveFn) {
+          auto const Call = ActiveFn->getActiveInstruction();
+          if (Call) {
+            moveToEndOfStmt(Unmapped, MappedModule, *Call);
+          }
+        }
+      }
+      else {
+        // A new function has been entered. Continue until we finish the
+        // prelude.
+        while (auto Next = trace::getNextInstructionInActiveFunction(Unmapped))
+          if (MappedModule.isMappedToStmt(*Next)
+              || !seec::trace::moveForward(Unmapped))
+            break;
+      }
+    }
+    
+    break;
+  }
+  
   Thread.getParent().cacheClear();
-  return Success;
+  
+  return Moved;
 }
 
 bool moveForwardToEnd(ThreadState &Thread) {
