a3a877815009b0cdcbed8feedb209018f7193835
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2013-10-14 13:52:14 +0800

    Make DIR information accessible from seec::cm. This is a thin wrapper, similar to the FILE stream information.

--- include/seec/Clang/MappedStreamState.hpp
+++ include/seec/Clang/MappedStreamState.hpp
@@ -26,6 +26,7 @@ namespace seec {
 
 namespace trace {
   class StreamState;
+  class DIRState;
 } // namespace trace (in seec)
 
 namespace cm {
@@ -63,6 +64,37 @@ public:
 /// \brief Print a textual description of a StreamState.
 ///
 llvm::raw_ostream &operator<<(llvm::raw_ostream &Out, StreamState const &State);
+
+
+/// \brief State of an open DIR.
+///
+class DIRState {
+  /// The base (unmapped) state.
+  seec::trace::DIRState const &UnmappedState;
+  
+public:
+  /// \brief Constructor.
+  ///
+  DIRState(seec::trace::DIRState const &ForUnmappedState);
+  
+  /// \name Accessors.
+  /// @{
+  
+  /// \brief Get the runtime address of the DIR.
+  ///
+  uintptr_t getAddress() const;
+  
+  /// \brief Get the pathname used when opening the DIR.
+  ///
+  std::string const &getDirname() const;
+  
+  /// @} (Accessors.)
+};
+
+
+/// \brief Print a textual description of a DIRState.
+///
+llvm::raw_ostream &operator<<(llvm::raw_ostream &Out, DIRState const &State);
 
 } // namespace cm (in seec)
 
