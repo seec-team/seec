93044e7f1104e68ea147081d6764a63af7ad6ec9
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2013-11-28 12:29:32 +0800

    Support chmod, fchmod, fstat, lstat, mkfifo, mknod, and umask.

--- lib/Runtimes/Tracer/WrapPOSIXsys_stat_h.cpp
+++ lib/Runtimes/Tracer/WrapPOSIXsys_stat_h.cpp
@@ -23,6 +23,86 @@ extern "C" {
 
 
 //===----------------------------------------------------------------------===//
+// chmod
+//===----------------------------------------------------------------------===//
+
+int
+SEEC_MANGLE_FUNCTION(chmod)
+(const char * const path, mode_t const mode)
+{
+  return
+    seec::SimpleWrapper
+      <seec::SimpleWrapperSetting::AcquireGlobalMemoryReadLock>
+      {seec::runtime_errors::format_selects::CStdFunction::chmod}
+      (chmod,
+       [](int const Result){ return Result == 0; },
+       seec::ResultStateRecorderForNoOp(),
+       seec::wrapInputCString(path),
+       mode);
+}
+
+
+//===----------------------------------------------------------------------===//
+// fchmod
+//===----------------------------------------------------------------------===//
+
+int
+SEEC_MANGLE_FUNCTION(fchmod)
+(int const fildes, mode_t const mode)
+{
+  return
+    seec::SimpleWrapper
+      <seec::SimpleWrapperSetting::AcquireGlobalMemoryReadLock>
+      {seec::runtime_errors::format_selects::CStdFunction::fchmod}
+      (fchmod,
+       [](int const Result){ return Result == 0; },
+       seec::ResultStateRecorderForNoOp(),
+       fildes,
+       mode);
+}
+
+
+//===----------------------------------------------------------------------===//
+// fstat
+//===----------------------------------------------------------------------===//
+
+int
+SEEC_MANGLE_FUNCTION(fstat)
+(int const fildes, struct stat * buf)
+{
+  return
+    seec::SimpleWrapper
+      <seec::SimpleWrapperSetting::AcquireGlobalMemoryWriteLock>
+      {seec::runtime_errors::format_selects::CStdFunction::fstat}
+      (fstat,
+       [](int const Result){ return Result == 0; },
+       seec::ResultStateRecorderForNoOp(),
+       fildes,
+       seec::wrapOutputPointer(buf));
+}
+
+
+//===----------------------------------------------------------------------===//
+// lstat
+//===----------------------------------------------------------------------===//
+
+int
+SEEC_MANGLE_FUNCTION(lstat)
+(const char * path, struct stat * buf)
+{
+  return
+    seec::SimpleWrapper
+      <seec::SimpleWrapperSetting::AcquireGlobalMemoryWriteLock>
+      {seec::runtime_errors::format_selects::CStdFunction::lstat}
+      (lstat,
+       [](int const Result){ return Result == 0; },
+       seec::ResultStateRecorderForNoOp(),
+       seec::wrapInputCString(path),
+       seec::wrapOutputPointer(buf));
+}
+
+
+//===----------------------------------------------------------------------===//
 // mkdir
 //===----------------------------------------------------------------------===//
 
@@ -39,6 +119,50 @@ SEEC_MANGLE_FUNCTION(mkdir)
        seec::ResultStateRecorderForNoOp(),
        seec::wrapInputCString(path),
        mode);
+}
+
+
+//===----------------------------------------------------------------------===//
+// mkfifo
+//===----------------------------------------------------------------------===//
+
+int
+SEEC_MANGLE_FUNCTION(mkfifo)
+(const char * const path, mode_t const mode)
+{
+  return
+    seec::SimpleWrapper
+      <seec::SimpleWrapperSetting::AcquireGlobalMemoryReadLock>
+      {seec::runtime_errors::format_selects::CStdFunction::mkfifo}
+      (mkfifo,
+       [](int const Result){ return Result == 0; },
+       seec::ResultStateRecorderForNoOp(),
+       seec::wrapInputCString(path),
+       mode);
+}
+
+
+//===----------------------------------------------------------------------===//
+// mknod
+//===----------------------------------------------------------------------===//
+
+int
+SEEC_MANGLE_FUNCTION(mknod)
+(const char * const path, mode_t const mode, dev_t const dev)
+{
+  // According to The Open Group Base Specifications (Issue 6), the behaviour
+  // of mknod() is unspecified if dev != 0. We might want to check that here to
+  // ensure that students are always writing portable code.
+  return
+    seec::SimpleWrapper
+      <seec::SimpleWrapperSetting::AcquireGlobalMemoryReadLock>
+      {seec::runtime_errors::format_selects::CStdFunction::mknod}
+      (mknod,
+       [](int const Result){ return Result == 0; },
+       seec::ResultStateRecorderForNoOp(),
+       seec::wrapInputCString(path),
+       mode,
+       dev);
 }
 
 
@@ -60,6 +184,12 @@ SEEC_MANGLE_FUNCTION(stat)
        seec::wrapInputCString(path),
        seec::wrapOutputPointer(buf));
 }
+
+
+//===----------------------------------------------------------------------===//
+// umask
+// Handled by include/seec/Transforms/FunctionsHandled.def
+//===----------------------------------------------------------------------===//
 
 
 } // extern "C"
