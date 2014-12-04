1000ec61d344c43019755767d3ea012ee69fa6bf
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2014-12-04 14:54:38 +0800

    Remove existing instrumented modules and execution traces before running tests.

--- tests/run_instrumented.sh
+++ tests/run_instrumented.sh
@@ -16,6 +16,14 @@ done
 program=$1
 shift
 
+if [ ! -z "$SEEC_WRITE_INSTRUMENTED" ] && [ -e $SEEC_WRITE_INSTRUMENTED ]; then
+  rm -f $SEEC_WRITE_INSTRUMENTED || true
+fi
+
+if [ ! -z "$SEEC_TRACE_NAME" ] && [ -e ${SEEC_TRACE_NAME}.seec ]; then
+  rm -f ${SEEC_TRACE_NAME}.seec || true
+fi
+
 if [ -z "$SEEC_TEST_SILENCE_STDOUT" ]
 then
   $program $* 1>/dev/null
