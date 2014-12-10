24c985098b5d4efc400d1d0362e4fb0c0054e7eb
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2014-12-10 14:21:30 +0800

    Add AnnotationPoint wrapper, which represents all annotations on a specific annotatable thing.
    Add methods to AnnotationCollection to retrieve the AnnotationPoint (if it
    exists) for a seec::cm::ThreadState, seec::cm::ProcessState, clang::Decl, or
    clang::Stmt.

--- tools/seec-trace-view/Annotations.cpp
+++ tools/seec-trace-view/Annotations.cpp
@@ -11,12 +11,44 @@
 ///
 //===----------------------------------------------------------------------===//
 
+#include "seec/Clang/MappedModule.hpp"
+#include "seec/Clang/MappedProcessState.hpp"
+#include "seec/Clang/MappedProcessTrace.hpp"
+#include "seec/Clang/MappedThreadState.hpp"
+#include "seec/Trace/ProcessState.hpp"
+#include "seec/Trace/ThreadState.hpp"
 #include "seec/Util/MakeUnique.hpp"
+#include "seec/wxWidgets/XmlNodeIterator.hpp"
 
 #include <wx/archive.h>
 #include <wx/xml/xml.h>
 
 #include "Annotations.hpp"
+
+using namespace seec;
+
+//------------------------------------------------------------------------------
+// AnnotationPoint
+//------------------------------------------------------------------------------
+
+bool AnnotationPoint::isForNode() const
+{
+  return m_Node->GetName() == "threadState";
+}
+
+bool AnnotationPoint::isForProcessState() const
+{
+  return m_Node->GetName() == "processState";
+}
+
+bool AnnotationPoint::isForThreadState() const
+{
+  return m_Node->GetName() == "threadState";
+}
+
+//------------------------------------------------------------------------------
+// AnnotationCollection
+//------------------------------------------------------------------------------
 
 namespace {
 
@@ -41,7 +73,7 @@ AnnotationCollection(std::unique_ptr<wxXmlDocument> XmlDocument)
 
 AnnotationCollection::AnnotationCollection()
 {
-  m_XmlDocument = seec::makeUnique<wxXmlDocument>();
+  m_XmlDocument = makeUnique<wxXmlDocument>();
 
   auto const Root = new wxXmlNode(nullptr, wxXML_ELEMENT_NODE, "annotations");
 
@@ -50,11 +82,11 @@ AnnotationCollection::AnnotationCollection()
 
 AnnotationCollection::~AnnotationCollection() = default;
 
-seec::Maybe<AnnotationCollection>
+Maybe<AnnotationCollection>
 AnnotationCollection::fromDoc(std::unique_ptr<wxXmlDocument> Doc)
 {
   if (!isAnnotationCollection(*Doc))
-    return seec::Maybe<AnnotationCollection>();
+    return Maybe<AnnotationCollection>();
 
   return AnnotationCollection(std::move(Doc));
 }
@@ -63,4 +95,140 @@ bool AnnotationCollection::writeToArchive(wxArchiveOutputStream &Stream)
 {
   return Stream.PutNextEntry("annotations.xml")
       && m_XmlDocument->Save(Stream);
+}
+
+Maybe<AnnotationPoint>
+AnnotationCollection::getPointForThreadState(cm::ThreadState const &State)
+{
+  auto const StateID = State.getThreadID();
+  auto const StateTime = State.getUnmappedState().getThreadTime();
+
+  auto const It = std::find_if(
+    wxXmlNodeIterator(m_XmlDocument->GetRoot()->GetChildren()),
+    wxXmlNodeIterator(),
+    [StateID, StateTime] (wxXmlNode const &Node) -> bool {
+      if (Node.GetName() != "threadState")
+        return false;
+
+      auto const StrID = Node.GetAttribute("thread");
+      unsigned long ID = 0;
+      if (!StrID.ToULong(&ID) || ID != StateID)
+        return false;
+
+      auto const StrTime = Node.GetAttribute("time");
+      unsigned long Time = 0;
+      if (!StrTime.ToULong(&Time) || Time != StateTime)
+        return false;
+
+      return true;
+    });
+
+  if (It != wxXmlNodeIterator())
+    return AnnotationPoint(*It);
+
+  return Maybe<AnnotationPoint>();
+}
+
+Maybe<AnnotationPoint>
+AnnotationCollection::getPointForProcessState(cm::ProcessState const &State)
+{
+  auto const StateTime = State.getUnmappedProcessState().getProcessTime();
+
+  auto const It = std::find_if(
+    wxXmlNodeIterator(m_XmlDocument->GetRoot()->GetChildren()),
+    wxXmlNodeIterator(),
+    [StateTime] (wxXmlNode const &Node) -> bool {
+      if (Node.GetName() != "processState")
+        return false;
+
+      auto const StrTime = Node.GetAttribute("time");
+      unsigned long Time = 0;
+      if (!StrTime.ToULong(&Time) || Time != StateTime)
+        return false;
+
+      return true;
+    });
+
+  if (It != wxXmlNodeIterator())
+    return AnnotationPoint(*It);
+
+  return Maybe<AnnotationPoint>();
+}
+
+namespace {
+
+Maybe<AnnotationPoint> getPointForNode(wxXmlNode &Root,
+                                       unsigned const ForASTIndex,
+                                       uint64_t const ForNodeIndex)
+{
+  auto const It = std::find_if(
+    wxXmlNodeIterator(Root.GetChildren()),
+    wxXmlNodeIterator(),
+    [ForASTIndex, ForNodeIndex] (wxXmlNode const &Node) -> bool {
+      if (Node.GetName() != "node")
+        return false;
+
+      auto const StrASTIndex = Node.GetAttribute("ASTIndex");
+      unsigned long ASTIndex = 0;
+      if (!StrASTIndex.ToULong(&ASTIndex) || ASTIndex != ForASTIndex)
+        return false;
+
+      auto const StrNodeIndex = Node.GetAttribute("nodeIndex");
+      unsigned long NodeIndex = 0;
+      if (!StrNodeIndex.ToULong(&NodeIndex) || NodeIndex != ForNodeIndex)
+        return false;
+
+      return true;
+    });
+
+  if (It != wxXmlNodeIterator())
+    return AnnotationPoint(*It);
+
+  return Maybe<AnnotationPoint>();
+}
+
+}
+
+Maybe<AnnotationPoint>
+AnnotationCollection::getPointForNode(cm::ProcessTrace const &Trace,
+                                      clang::Decl const *Node)
+{
+  auto const &Mapping = Trace.getMapping();
+  auto const AST = Mapping.getASTForDecl(Node);
+  if (!AST)
+    return Maybe<AnnotationPoint>();
+
+  auto const MaybeIndex = AST->getIdxForDecl(Node);
+  if (!MaybeIndex.assigned<uint64_t>())
+    return Maybe<AnnotationPoint>();
+
+  auto const MaybeASTIndex = Mapping.getASTIndex(AST);
+  if (!MaybeASTIndex.assigned(0))
+    return Maybe<AnnotationPoint>();
+
+  return ::getPointForNode(*(m_XmlDocument->GetRoot()),
+                           MaybeASTIndex.get<0>(),
+                           MaybeIndex.get<uint64_t>());
+}
+
+Maybe<AnnotationPoint>
+AnnotationCollection::getPointForNode(cm::ProcessTrace const &Trace,
+                                      clang::Stmt const *Node)
+{
+  auto const &Mapping = Trace.getMapping();
+  auto const AST = Mapping.getASTForStmt(Node);
+  if (!AST)
+    return Maybe<AnnotationPoint>();
+
+  auto const MaybeIndex = AST->getIdxForStmt(Node);
+  if (!MaybeIndex.assigned<uint64_t>())
+    return Maybe<AnnotationPoint>();
+
+  auto const MaybeASTIndex = Mapping.getASTIndex(AST);
+  if (!MaybeASTIndex.assigned(0))
+    return Maybe<AnnotationPoint>();
+
+  return ::getPointForNode(*(m_XmlDocument->GetRoot()),
+                           MaybeASTIndex.get<0>(),
+                           MaybeIndex.get<uint64_t>());
 }
--- tools/seec-trace-view/Annotations.hpp
+++ tools/seec-trace-view/Annotations.hpp
@@ -18,8 +18,49 @@
 
 #include <memory>
 
+namespace seec {
+  namespace cm {
+    class ProcessState;
+    class ProcessTrace;
+    class ThreadState;
+  }
+}
+
+namespace clang {
+  class Decl;
+  class Stmt;
+}
+
 class wxArchiveOutputStream;
 class wxXmlDocument;
+class wxXmlNode;
+
+/// \brief Represents a single annotation point.
+/// This might be for a particular AST node, a process state, or a thread state.
+///
+class AnnotationPoint final
+{
+  wxXmlNode *m_Node;
+
+public:
+  /// \brief Construct a new \c AnnotationPoint wrapping the given XML node.
+  ///
+  AnnotationPoint(wxXmlNode &ForNode)
+  : m_Node(&ForNode)
+  {}
+
+  /// \brief Check if this point is for a \c seec::cm::ThreadState.
+  ///
+  bool isForThreadState() const;
+
+  /// \brief Check if this point is for a \c seec::cm::ProcessState.
+  ///
+  bool isForProcessState() const;
+
+  /// \brief Check if this point is for an AST node.
+  ///
+  bool isForNode() const;
+};
 
 /// \brief Holds all annotations for an execution trace.
 ///
@@ -63,6 +104,26 @@ public:
   /// \brief Write to "annotations.xml" in the given archive.
   ///
   bool writeToArchive(wxArchiveOutputStream &Stream);
+
+  /// \brief Get the \c AnnotationPoint for a \c ThreadState (if it exists).
+  ///
+  seec::Maybe<AnnotationPoint>
+  getPointForThreadState(seec::cm::ThreadState const &);
+
+  /// \brief Get the \c AnnotationPoint for a \c ProcessState (if it exists).
+  ///
+  seec::Maybe<AnnotationPoint>
+  getPointForProcessState(seec::cm::ProcessState const &);
+
+  /// \brief Get the \c AnnotationPoint for a Decl (if it exists).
+  ///
+  seec::Maybe<AnnotationPoint> getPointForNode(seec::cm::ProcessTrace const &,
+                                               clang::Decl const *);
+
+  /// \brief Get the \c AnnotationPoint for a Stmt (if it exists).
+  ///
+  seec::Maybe<AnnotationPoint> getPointForNode(seec::cm::ProcessTrace const &,
+                                               clang::Stmt const *);
 };
 
 #endif // SEEC_TRACE_VIEW_ANNOTATIONS_HPP
