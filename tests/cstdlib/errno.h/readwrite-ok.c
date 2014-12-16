ffbfdd79812d36b9aae0ba26ef3bfb04d21a0c2d
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2014-12-16 13:58:32 +0800

    Make these tests more portable.

--- tests/cstdlib/errno.h/readwrite-ok.c
+++ tests/cstdlib/errno.h/readwrite-ok.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("errno: %d\n", errno);
   errno = 0;
   printf("errno: %d\n", errno);
   return 0;
--- tests/cstdlib/islower/ok-printarg.c
+++ tests/cstdlib/islower/ok-printarg.c
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-  for (int i = 0; argv[0][i]; ++i)
-    if (islower(argv[0][i]))
-      printf("%c\n", argv[0][i]);
+  for (int i = 0; argv[1][i]; ++i)
+    if (islower(argv[1][i]))
+      printf("%c\n", argv[1][i]);
 
   return 0;
 }
