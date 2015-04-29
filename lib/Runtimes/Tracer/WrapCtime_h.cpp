3a9adfd4e7996df523414c99e59f8f1769295f95
Matthew Heinsen Egan <mheinsen@iinet.net.au>
2015-04-29 20:33:12 +0800

    Support clock, difftime, and timespec_get.

--- lib/Runtimes/Tracer/WrapCtime_h.cpp
+++ lib/Runtimes/Tracer/WrapCtime_h.cpp
@@ -48,6 +48,27 @@ SEEC_MANGLE_FUNCTION(time)
 
 
 //===----------------------------------------------------------------------===//
+// timespec_get
+//===----------------------------------------------------------------------===//
+
+int
+SEEC_MANGLE_FUNCTION(timespec_get)
+(struct timespec *ts, int base)
+{
+  // Use the SimpleWrapper mechanism.
+  return
+    seec::SimpleWrapper
+      <seec::SimpleWrapperSetting::AcquireGlobalMemoryWriteLock>
+      {seec::runtime_errors::format_selects::CStdFunction::timespec_get}
+      (timespec_get,
+       [](int Result){ return Result != 0; },
+       seec::ResultStateRecorderForNoOp(),
+       seec::wrapOutputPointer(ts).setIgnoreNull(false),
+       base);
+}
+
+
+//===----------------------------------------------------------------------===//
 // asctime
 //===----------------------------------------------------------------------===//
 
