3749b3a709b8ecd50edbf5a70084de1524e3b2a1
Matthew Heinsen Egan <matthew.heinsen.egan@gmail.com>
2016-04-12 15:40:12 +0900

    Update printing for the Online Python Tutor trace format.
    Online Python Tutor has upcoming support for C/C++ visualization. The front-end
    now supports some C/C++ features which the old trace format couldn't handle,
    e.g. arrays and structs allocated on the stack, and pointers to sub-objects of
    heap allocations.
    
    This commit updates SeeC's OPT trace format output to use these new features.
    Also, runtime errors are now expressed in the trace (using OPT's support for
    exceptions).

--- tools/seec-trace-print/OnlinePythonTutor.hpp
+++ tools/seec-trace-print/OnlinePythonTutor.hpp
@@ -14,24 +14,46 @@
 #ifndef SEEC_TRACE_PRINT_ONLINEPYTHONTUTOR_HPP
 #define SEEC_TRACE_PRINT_ONLINEPYTHONTUTOR_HPP
 
+#include "llvm/ADT/StringRef.h"
+
+#include <string>
+
 namespace seec {
   namespace cm {
     class ProcessTrace;
   }
+  class AugmentationCollection;
 }
 
 class OPTSettings {
+  seec::AugmentationCollection const &Augmentations;
+
   bool PyCrazyMode;
 
+  std::string VariableName;
+
 public:
-  OPTSettings()
-  : PyCrazyMode(false)
+  OPTSettings(seec::AugmentationCollection const &WithAugmentations)
+  : Augmentations(WithAugmentations),
+    PyCrazyMode(false),
+    VariableName()
   {}
+
+  seec::AugmentationCollection const &getAugmentations() const {
+    return Augmentations;
+  }
 
   bool getPyCrazyMode() const { return PyCrazyMode; }
 
   OPTSettings &setPyCrazyMode(bool const Value) {
     PyCrazyMode = Value;
+    return *this;
+  }
+
+  std::string const &getVariableName() const { return VariableName; }
+
+  OPTSettings &setVariableName(llvm::StringRef Value) {
+    VariableName = Value;
     return *this;
   }
 };
